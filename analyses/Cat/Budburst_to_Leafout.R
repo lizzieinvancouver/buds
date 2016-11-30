882309cc33991524d1ce7c2258c47c62ea385413
cchambe12 <CatherineChamberlain@Catherines-MacBook-Pro-2.local>
2016-11-30 16:38:30 -0500

    simple timeline graph for hf and sh

--- analyses/Cat/Budburst_to_Leafout.R
+++ analyses/Cat/Budburst_to_Leafout.R
@@ -44,6 +44,52 @@ hf<-na.omit(hf)
 hf$Risk <- hf$Leaves - hf$Budburst
 hf<-filter(hf, Risk > 0)
 
+hf.buds <- hf %>%
+  filter(treatcode == "CS0") %>%
+  select(sp,id,Budburst) %>%
+  group_by(sp) %>%
+  summarise_each(funs(mean), Budburst)
+
+hf.leaves <- hf %>%
+  filter(treatcode == "CS0") %>%
+  select(sp,id,Leaves) %>%
+  group_by(sp) %>%
+  summarise_each(funs(mean), Leaves)
+
+hf.Risk <- hf %>%
+  filter(treatcode == "CS0") %>%
+  select(sp,id,Risk) %>%
+  group_by(sp) %>%
+  summarise_each(funs(mean), Risk)
+
+cold<-full_join(hf.buds, hf.leaves)
+cold<-full_join(cold, hf.Risk)
+cold$tx<-"CS0"
+
+warm.buds <- hf %>%
+  filter(treatcode == "WL2") %>%
+  select(sp,id,Budburst) %>%
+  group_by(sp) %>%
+  summarise_each(funs(mean), Budburst)
+
+warm.leaves <- hf %>%
+  filter(treatcode == "WL2") %>%
+  select(sp,id,Leaves) %>%
+  group_by(sp) %>%
+  summarise_each(funs(mean), Leaves)
+
+warm.Risk <- hf %>%
+  filter(treatcode == "WL2") %>%
+  select(sp,id,Risk) %>%
+  group_by(sp) %>%
+  summarise_each(funs(mean), Risk)
+
+warm<-full_join(warm.buds, warm.leaves)
+warm<-full_join(warm, warm.Risk)
+warm$tx<-"WL2"
+
+hf<-bind_rows(warm,cold)
+ ######## Saint Hipp Data ########
 sh<- timeline%>%
   filter(site == "SH") %>%
   group_by(sp, id, tleaf) %>%
@@ -54,20 +100,64 @@ sh<-na.omit(sh)
 sh$Risk <- sh$Leaves - sh$Budburst
 sh<-filter(sh, Risk > 0)
 
+sh.buds <- sh %>%
+  filter(treatcode == "CS0") %>%
+  select(sp,id,Budburst) %>%
+  group_by(sp) %>%
+  summarise_each(funs(mean), Budburst)
+
+sh.leaves <- sh %>%
+  filter(treatcode == "CS0") %>%
+  select(sp,id,Leaves) %>%
+  group_by(sp) %>%
+  summarise_each(funs(mean), Leaves)
+
+sh.Risk <- sh %>%
+  filter(treatcode == "CS0") %>%
+  select(sp,id,Risk) %>%
+  group_by(sp) %>%
+  summarise_each(funs(mean), Risk)
+
+cold.sh<-full_join(sh.buds, sh.leaves)
+cold.sh<-full_join(cold.sh, sh.Risk)
+cold.sh$tx<-"CS0"
+
+warm.buds.sh <- sh %>%
+  filter(treatcode == "WL2") %>%
+  select(sp,id,Budburst) %>%
+  group_by(sp) %>%
+  summarise_each(funs(mean), Budburst)
+
+warm.leaves.sh <- sh %>%
+  filter(treatcode == "WL2") %>%
+  select(sp,id,Leaves) %>%
+  group_by(sp) %>%
+  summarise_each(funs(mean), Leaves)
+
+warm.Risk.sh <- sh %>%
+  filter(treatcode == "WL2") %>%
+  select(sp,id,Risk) %>%
+  group_by(sp) %>%
+  summarise_each(funs(mean), Risk)
+
+warm.sh<-full_join(warm.buds.sh, warm.leaves.sh)
+warm.sh<-full_join(warm.sh, warm.Risk.sh)
+warm.sh$tx<-"WL2"
+
+sh<-bind_rows(warm.sh,cold.sh)
+
 
 ## from other script...
 #basic$Risk<- basic$Leaves - basic$Budburst
 
-ggplot((hf), aes(x=Budburst, y=id)) + geom_point(aes(x= hf$Budburst)) + 
-  geom_segment(aes(y = id, yend = id, x = Budburst, xend = Leaves)) + 
-  geom_point(aes(x=hf$Leaves)) +
-  geom_point(position = position_dodge(.5)) + geom_point(aes(col=treatcode)) +
+ggplot((hf), aes(x=Budburst, y=sp)) + geom_point(aes(x= hf$Budburst)) + 
+  geom_segment(aes(y = sp, yend = sp, x = Budburst, xend = Leaves)) + 
+  geom_point(aes(x=hf$Leaves)) + geom_point(aes(col=tx)) +
   xlab("Budburst to Leaf Out") +
   ylab("Species")
 
-ggplot((sh), aes(x=Budburst, y=id)) + geom_point(aes(x= sh$Budburst)) + 
-  geom_segment(aes(y = id, yend = id, x = Budburst, xend = Leaves)) + 
-  geom_point(aes(x=sh$Leaves)) +
-  geom_point(position = position_dodge(.5)) + geom_point(aes(col=treatcode)) +
+ggplot((sh), aes(x=Budburst, y=sp)) + geom_point(aes(x= sh$Budburst)) + 
+  geom_segment(aes(y = sp, yend = sp, x = Budburst, xend = Leaves)) + 
+  geom_point(aes(x=sh$Leaves))  + geom_point(aes(col=tx)) +
   xlab("Budburst to Leaf Out") +
   ylab("Species")
