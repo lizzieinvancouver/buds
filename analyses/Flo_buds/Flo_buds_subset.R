8e6678f2cc2467259eb8e81368d349543f0a7c06
dbuona <dbuonaiuto@g.harvard.edu>
2016-12-08 20:35:30 -0500

    more flo_bud graphing
    I broke my code, but Ill try again later

--- analyses/Flo_buds/Flo_buds_subset.R
+++ analyses/Flo_buds/Flo_buds_subset.R
@@ -3,7 +3,7 @@
 rm(list=ls()) # remove everything currently held in the R memory
 options(stringsAsFactors=FALSE)
 graphics.off()
-
+library(plotrix)
 library(gdata)
 library(nlme)
 library(scales)
@@ -16,96 +16,36 @@ library(ggplot2)
 
 setwd("~/Documents/git/buds/analyses/input")
 fb<-read.csv("Budburst By Day.csv", header = TRUE)
-#########################
-#metrics for flowering
-#########################
-nrow(fb)
-# total rows= 2136
-factor(fb$sp)
-totsp<-table(fb$sp)
-##28 total species in fb data sheet
+View(fb)
 
-##now find how many rows have entries for Fday (day of first flowering)
-reduced1<-filter(fb, !is.na(fday))
-nrow(reduced1)
-#216 so only 216/2136 flowered at all.
-##How many species
-factor(reduced1$sp)
-flosp<-table(reduced1$sp)
-##15 species flowered so, 15/28 species flowers
-##of those entries how many species had >10 individuals flower?
-cnt<-count(reduced1, sp)
-cnt
-tenplus<-filter(cnt,n>10)
-tenplus
-#seven species have >10 flowering episodes:
-#1 ACEPEN    11
-#2 ACERUB    23
-#3 CORCOR    23
-#4 ILEMUC    60
-#5 POPGRA    25
-#6 PRUPEN    28
-#7 VIBLAN    13
-#How many species total leafed out
-fb2<-filter(fb,nl==1)
-nrow(fb2)
-##1704/2136 speices leafed out
-##How many burstbuds?
-fb3<-filter(fb,!is.na(bday))
-nrow(fb3)
-#1926/2136 broke buds
-#How many flowered but didnt leaf out?
-fb4<-filter(fb,is.na(lday) & !is.na(fday))
-nrow(fb4)
-#22/2136 flowered but didnt leafout
-##Did any flower but not break buds?
-fb5<-filter(fb,is.na(bday) & !is.na(fday))
-nrow(fb5)
-#6/2136 flowered without breaking buds
-## perhaps how many cutting had no activity
-fb5<-filter(fb,is.na(bday) & is.na(fday)&is.na(lday))
-nrow(fb5)
-##204/2136 showen no activity
-### how many flowers/species entries are therem
-ace_pentotal<-filter(fb, sp=="ACEPEN")
-nrow(ace_pentotal)
-ace_rubtotal<-filter(fb, sp=="ACERUB")
-nrow(ace_rubtotal)
-cortotal<-filter(fb, sp=="CORCOR")
-nrow(cortotal)
-iltotal<-filter(fb, sp=="ILEMUC")
-nrow(iltotal)
-poptotal<-filter(fb, sp=="POPGRA")
-nrow(poptotal)
-prutotal<-filter(fb, sp=="PRUPEN")
-nrow(prutotal)
-vib_lantotal<-filter(fb, sp=="VIBLAN")
-nrow(vib_lantotal)
-
-#1 ACEPEN    11/144
-#2 ACERUB    23/132
-#3 CORCOR    23/48
-#4 ILEMUC    60/144
-#5 POPGRA    25/132
-#6 PRUPEN    28/56
-#7 VIBLAN    13/144
-
-### all metrics are copied into Flo_buds_metrics.csv
-
-#################################
-#if you wanted to, you could subset data to include only entries with flowering, budburst and leaf out observations but this is less useful
-#reduced<-filter(fb,nl==1 & !is.na(fday))
-#write.csv(reduced, "Flo_bud_ds.csv", row.names=FALSE)
-#write.csv(reduced1, "Flo_bud_ds_allflo.csv", row.names=FALSE)
-###################################
 #make subset for only species with 10+ flowering episodes
 mag7<-filter(fb, sp %in% c("ACEPEN", "ACERUB", "CORCOR","ILEMUC", "POPGRA", "PRUPEN", "VIBLAN"))
 View(mag7)
 nrow(mag7) #800
 ##now lets plot the results for 1 treatment WL0
-mag7_WLO<-filter(mag7,treatcode=="WL0")
-count(mag7_WLO,!is.na(fday))##make sure there are flowering entries in this subset (FALSE =56 TRUE=28)
-###now I want to make a new variable that is mean fday and lday seperated by species, but I am going to eat lunch
-grouped<-group_by(mag7_WLO, sp)
-summarise(grouped, mean=mean(fday), sd=sd(fday))
-summarise(grouped, mean=mean(lday), sd=sd(lday))
+mag7_WL0<-filter(mag7,treatcode=="WL0")
+View(mag7_WL0)
+##### THe code broke here it worked before but the grouping doesnt seem to be working.
+grouped<-group_by(mag7_WL0,sp)
+
+##calculate mean ## decided to try making each an object instead
+fdaysum<-summarise(grouped, meanf=mean(fday,na.rm=TRUE),sef=sd(fday,na.rm=TRUE)/sqrt(length(fday)), meanl=mean(lday,na.rm=TRUE), sel=sd(lday,na.rm=TRUE)/sqrt(length(lday)))
+View(fdaysum)
+try<-ggplot(fdaysum, aes(x=sp, y=meanf))
+try+geom_point(aes(sp,meanf, color="blue"))+geom_pointrange(aes(ymin=meanf-sef, ymax=meanf+sef, color="blue"))
+
+###plot WLO
+plot<-ggplot(fdaysum, aes(sp,day))
+plot+geom_point(aes(sp,meanf, color="blue"))+ geom_point(aes(sp,meanl,color="red"))+ggtitle("Treatment WL0")
+##now do this for CS0
+mag7_CS0<-filter(mag7,treatcode=="CS0")
+grouped2<-group_by(mag7_CS0, sp)
+fdaysum2<-summarise(grouped2, meanf=mean(fday,na.rm=TRUE),sef=sd(fday,na.rm=TRUE)/sqrt(length(fday)), meanl=mean(lday,na.rm=TRUE), sel=sd(lday,na.rm=TRUE)/sqrt(length(lday)))
+plot2<-ggplot(fdaysum2, aes(sp,day))
+plot2+geom_point(aes(sp,meanf, color="blue"))+ geom_point(aes(sp,meanl,color="red"))+ggtitle("Treatment CS0")
+#Now WS0
+mag7_WS0<-filter(mag7,treatcode=="WS0")
+grouped3<-group_by(mag7_WS0, sp)
+fdaysum3<-summarise(grouped3, meanf=mean(fday,na.rm=TRUE),sef=sd(fday,na.rm=TRUE)/sqrt(length(fday)), meanl=mean(lday,na.rm=TRUE), sel=sd(lday,na.rm=TRUE)/sqrt(length(lday)))
+plot3<-ggplot(fdaysum2, aes(sp,day))
+plot3+geom_point(aes(sp,meanf, color="blue"))+ geom_point(aes(sp,meanl,color="red"))+ggtitle("Treatment WS0")
---
+++ analyses/Flo_buds/flo_bud_metrics.R
@@ -0,0 +1,78 @@
+#metrics for flowering
+#########################
+nrow(fb)
+# total rows= 2136
+factor(fb$sp)
+totsp<-table(fb$sp)
+##28 total species in fb data sheet
+
+##now find how many rows have entries for Fday (day of first flowering)
+reduced1<-filter(fb, !is.na(fday))
+nrow(reduced1)
+#216 so only 216/2136 flowered at all.
+##How many species
+factor(reduced1$sp)
+flosp<-table(reduced1$sp)
+##15 species flowered so, 15/28 species flowers
+##of those entries how many species had >10 individuals flower?
+cnt<-count(reduced1, sp)
+cnt
+tenplus<-filter(cnt,n>10)
+tenplus
+#seven species have >10 flowering episodes:
+#1 ACEPEN    11
+#2 ACERUB    23
+#3 CORCOR    23
+#4 ILEMUC    60
+#5 POPGRA    25
+#6 PRUPEN    28
+#7 VIBLAN    13
+#How many species total leafed out
+fb2<-filter(fb,nl==1)
+nrow(fb2)
+##1704/2136 speices leafed out
+##How many burstbuds?
+fb3<-filter(fb,!is.na(bday))
+nrow(fb3)
+#1926/2136 broke buds
+#How many flowered but didnt leaf out?
+fb4<-filter(fb,is.na(lday) & !is.na(fday))
+nrow(fb4)
+#22/2136 flowered but didnt leafout
+##Did any flower but not break buds?
+fb5<-filter(fb,is.na(bday) & !is.na(fday))
+nrow(fb5)
+#6/2136 flowered without breaking buds
+## perhaps how many cutting had no activity
+fb5<-filter(fb,is.na(bday) & is.na(fday)&is.na(lday))
+nrow(fb5)
+##204/2136 showen no activity
+### how many flowers/species entries are therem
+ace_pentotal<-filter(fb, sp=="ACEPEN")
+nrow(ace_pentotal)
+ace_rubtotal<-filter(fb, sp=="ACERUB")
+nrow(ace_rubtotal)
+cortotal<-filter(fb, sp=="CORCOR")
+nrow(cortotal)
+iltotal<-filter(fb, sp=="ILEMUC")
+nrow(iltotal)
+poptotal<-filter(fb, sp=="POPGRA")
+nrow(poptotal)
+prutotal<-filter(fb, sp=="PRUPEN")
+nrow(prutotal)
+vib_lantotal<-filter(fb, sp=="VIBLAN")
+nrow(vib_lantotal)
+
+#1 ACEPEN    11/144
+#2 ACERUB    23/132
+#3 CORCOR    23/48
+#4 ILEMUC    60/144
+#5 POPGRA    25/132
+#6 PRUPEN    28/56
+#7 VIBLAN    13/144
+
+### all metrics are copied into Flo_buds_metrics.csv
+#if you wanted to, you could subset data to include only entries with flowering, budburst and leaf out observations but this is less useful
+#reduced<-filter(fb,nl==1 & !is.na(fday))
+#write.csv(reduced, "Flo_bud_ds.csv", row.names=FALSE)
+#write.csv(reduced1, "Flo_bud_ds_allflo.csv", row.names=FALSE)
