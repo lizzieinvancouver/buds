be76ca1a453e67135b5e9e9420ac70412d7ac9ca
Elizabeth Wolkovich <wolkovich@biodiversity.edu>
2018-02-16 16:08:12 -0800

    Updating chilling calcs in supp

--- analyses/chill unit calcs.R
+++ analyses/chill unit calcs.R
@@ -6,32 +6,48 @@ setwd("/Users/Lizzie/Documents/git/projects/treegarden/budexperiments/analyses")
 toload <- sort(dir("./input")[grep("Budburst Data", dir('./input'))], T)[1]
 load(file.path("input", toload))
 
+loadHFdata <- TRUE
 
 # Dynamic Model
 
 library(chillR)
 # Luedeling recommends Dynamic Model (chill portions)
 
-if(FALSE){ # Dan had this commented out but I believe it to be where he builds the HF data ... 
+if(!loadHFdata){ # Dan had this commented out but it is where he builds the HF data ... so I adjusted it
 # HF chilling from Oct 1 2014
- 
 htemp <- read.csv("..//data/hf001-10-15min-m.csv")
 
 htemp$datetime <- as.POSIXlt(htemp$datetime, format = "%Y-%m-%dT%H:%M")
 
 # select years. There are 44 na's
- 
-htemp1 <- htemp[htemp$datetime > "2014-09-30" & htemp$datetime < "2015-03-30" & !is.na(htemp$datetime),]
- 
+htemp1 <- htemp[htemp$datetime > "2014-09-30" & htemp$datetime < "2015-05-31" & !is.na(htemp$datetime),]
 htemp2 <- htemp[htemp$datetime > "2015-11-01" & !is.na(htemp$datetime),]
 
 # aggregate to hourly averages 
 ht <- aggregate(airt ~ format(htemp1$datetime, "%Y-%m-%d %H"), mean, data = htemp1)
 names(ht)[1] = 'datetime'
-# save(file = "HF Temp Data.Rdata", list=c("ht", "htemp1", "htemp2")) # to save a lot of time in Sweaving
+save(file = "HF Temp Data to May.Rdata", list=c("ht", "htemp1", "htemp2"))
 }
-    
-load("HF Temp Data.Rdata")
+
+if(FALSE){ # This is Dan's original code, mine above extends to end of May which seems to have no effect on answers and lets me look at chilling later in year
+htemp <- read.csv("..//data/hf001-10-15min-m.csv")
+
+htemp$datetime <- as.POSIXlt(htemp$datetime, format = "%Y-%m-%dT%H:%M")
+
+# select years. There are 44 na's
+htemp1 <- htemp[htemp$datetime > "2014-09-30" & htemp$datetime < "2015-01-22" & !is.na(htemp$datetime),]
+htemp2 <- htemp[htemp$datetime > "2015-11-01" & !is.na(htemp$datetime),]
+
+# aggregate to hourly averages 
+ht <- aggregate(airt ~ format(htemp1$datetime, "%Y-%m-%d %H"), mean, data = htemp1)
+names(ht)[1] = 'datetime'
+save(file = "HF Temp Data.Rdata", list=c("ht", "htemp1", "htemp2")) # to save a lot of time in Sweaving
+    }
+
+if(loadHFdata){
+# load("HF Temp Data.Rdata")
+load("HF Temp Data to May.Rdata")
+}
 
 ht$Year <- as.numeric(substr(ht$datetime, 1, 4))
 ht$JDay <- as.numeric(format(strptime(substr(ht$datetime, 1, 10), "%Y-%m-%d"), "%j"))
@@ -49,14 +65,15 @@ names(ht2)[2] = "Temp"
 
 chill0calc <- chilling(ht2, 273, 21) # 56 chill portions by Jan 21 last year.
 
-# What about by March 1?
-chill1Mar <- chilling(ht2, 273, 60) # 57 chill portions by 1 March.
-chill130 <- chilling(ht2, 273, 130) # 77.5 chill portions by 10 May (see Richardonson et al. 2009, Tree Phys)
+# What about by Mar 1, April 1 or May 10?
+chill60 <- chilling(ht2, 273, 60) # 57 chill portions by 1 March.
+chill91 <- chilling(ht2, 273, 91) # 79 chill portions by 1 April.
+chill121 <- chilling(ht2, 273, 121) # 100 chill portions by 1 May (10 May is another option, see Richardonson et al. 2009, Tree Phys)
 
+# Helpful code to convert dates ... 
 jtomod <- as.Date("130", format = "%j")
 format(jtomod, "%d-%b")
-
-modtoj <- as.Date("Mar-01", format = "%b-%d")
+modtoj <- as.Date("Apr-01", format = "%b-%d")
 format(modtoj, "%j")
 
 # Chilling in experimental setting. From Jan 22 - Feb 12 at 4, then to either 4 or 1.5 for additional 30 d. total: 22 d at 4, then 30 d extra.
@@ -83,9 +100,10 @@ chill2calc <- chilling(chill2, 0, 80) # FEWER chill portions and fewer Utah Mode
 
 colz = c("Chilling_Hours","Utah_Model","Chill_portions")
 
-allcalc <- rbind(chill0calc[colz], chill0calc[colz]+chill1calc[colz], chill0calc[colz]+chill2calc[colz])
+allcalc <- rbind(chill0calc[colz], chill91[colz], chill121[colz],
+    chill0calc[colz]+chill1calc[colz], chill0calc[colz]+chill2calc[colz])
 
-hfallcalc <- data.frame(Treatment = c("Field chilling","4.0° x 30 d", "1.5° x 30 d"), allcalc)
+hfallcalc <- data.frame(Treatment = c("Field chilling", "Field chilling to 1 Apr", "Field chilling to 1 May", "Field chill + 4.0° x 30 d", "Field chill + 1.5° x 30 d"), allcalc)
 rownames(hfallcalc)=NULL
 
 ## St. Hipp data 
@@ -139,12 +157,12 @@ for(i in 1:nrow(stemp)){
 st <- hrly
 st <- st[st$date > "2014-08-30" & st$date < "2015-03-01",]
 
-stmay <- st[st$date > "2014-08-30" & st$date < "2015-05-31",]
+stmay <- hrly[hrly$date > "2014-08-30" & hrly$date < "2015-05-31",]
 
 chill0calc.SH <- chilling(st, 273, 21) # 39 chill portions by Jan 20 last year.
-mar1calc.SH <- chilling(stmay, 273, 60) # 
-may10calc.SH <- chilling(stmay, 273, 130)
-
+calc.SH60 <- chilling(stmay, 273, 60) # 44
+calc.SH91 <- chilling(stmay, 273, 91) # 64
+calc.SH121 <- chilling(stmay, 273, 121) # 84 chill portions
 
 colz = c("Chilling_Hours","Utah_Model","Chill_portions")
 
@@ -156,8 +174,17 @@ allcalc <- rbind(chill0calc[colz], chill0calc[colz]+chill1calc[colz], chill0calc
 allcalc <- data.frame(Site = c("Harvard Forest", "","","St. Hippolyte","",""), Treatment = rep(c("Field chilling","4.0° x 30 d", "1.5° x 30 d"), 2), allcalc)
 rownames(allcalc)=NULL
 
+allcalc <- rbind(chill0calc[colz], chill91[colz], chill121[colz],
+    chill0calc[colz]+chill1calc[colz], chill0calc[colz]+chill2calc[colz],
+    chill0calc.SH[colz], calc.SH91[colz], calc.SH121[colz],
+    chill0calc.SH[colz]+chill1calc[colz], chill0calc.SH[colz]+chill2calc[colz])
 
+allcalc <- data.frame(Site = c("Harvard Forest", "","","", "", "St. Hippolyte","","", "", ""),
+    Treatment = rep(c("Field chilling until collection","Field chilling to 1 Apr",
+       "Field chilling to 1 May", "Field chilling + 4.0° x 30 d",
+       "Field chilling + 1.5° x 30 d"), 2), allcalc)
 
+rownames(allcalc)=NULL
 
 # Three models: Chilling Hours, Utah, Dynamic. Chilling hours: all temperatures equally effective (0-7.2 deg C). 
 # Utah: Richardson EA, Seeley SD, Walker DR (1974) A model for estimating the completion of rest for Redhaven and Elberta peach trees. Hortscience 9(4):331–332
